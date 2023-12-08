fb9cdceb6dfcafe743e2de9246b662032cdcc28c
anton <sparco114@ya.ru>
2023-12-08 12:51:24 +0300

    - changed SECRET_KEY

--- velo_backend/conf/settings.py
+++ velo_backend/conf/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os.path
+import os
 from pathlib import Path
+
+import dotenv
+
+dotenv.load_dotenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&sl!@5%er3)ql%**gbpjwn4dnh0z&9mi&k(@yi32b)tl$up6*^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -164,8 +168,8 @@ REST_FRAMEWORK = {
     #     'rest_framework.renderers.JSONRenderer',
     # ),
     # 'DEFAULT_PARSER_CLASSES': (
-        # 'rest_framework.parsers.JSONParser',
-        # 'rest_framework.parsers.MultiPartParser',
+    # 'rest_framework.parsers.JSONParser',
+    # 'rest_framework.parsers.MultiPartParser',
     # ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
